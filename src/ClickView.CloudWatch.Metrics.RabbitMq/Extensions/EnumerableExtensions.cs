5b8d4b21c95f86686718b85c2e766d96e846feae
Kevin <kevin.well@clickview.com.au>
2020-06-30 15:30:21 +1000

    Batch the metrics to decrease the request size of CloudWatch calls

---
+++ src/ClickView.CloudWatch.Metrics.RabbitMq/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace ClickView.CloudWatch.Metrics.RabbitMq.Extensions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<IEnumerable<T>> GroupInto<T>(this IEnumerable<T> list, int size)
+        {
+            if (list == null)
+                throw new ArgumentNullException(nameof(list));
+            
+            return list.Select((v, i) => new { Index = i, Value = v })
+                .GroupBy(m => m.Index / size)
+                .Select(s => s.Select(v => v.Value));
+        }
+    }
+}
